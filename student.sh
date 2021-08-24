b17de63388df1b48ee0c200966692eebd6d57531
riteshshah11 <rites84@gmail.com>
2021-08-24 18:34:55 +0530

    student file with data

---
+++ student.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#the following script is for taking input from a student and display related contents.
+echo "Please enter student name: "
+read PERSON
+echo "Hello, ${PERSON}"
+echo "Please enter student ID: "
+read ID
+echo "The student ID is : ${ID}"
+echo "The student name is ${PERSON} and the id is ${ID}"
